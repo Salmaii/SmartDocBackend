b3ffe7ea72da21a8de1cb3314f33d9896ced0883
Luís Eduardo <anjos.luiseduardo@gmail.com>
2021-04-21 20:11:15 -0300

    criacao da classe consulta

---
+++ src/main/kotlin/Consulta.kt
@@ -0,0 +1,10 @@
+import Pessoa.Medico.Medico
+import Pessoa.Paciente.Paciente
+
+class Consulta{
+    var hora: String? = null
+    var data: String? = null
+    var local: Local? = null
+    var medico: Medico? = null
+    var paciente: Paciente? = null
+}
